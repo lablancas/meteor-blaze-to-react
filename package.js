c2c6d300004322b4076fc551b7a67c3d00f33267
Lucas Blancas <lablancas@gmail.com>
2017-04-12 10:49:02 -0500

    upgrade to meteor v1.4 and add npm dependencies

--- package.js
+++ package.js
@@ -1,13 +1,24 @@
 Package.describe({
-  name: 'gwendall:blaze-to-react',
+  name: 'lablancas:blaze-to-react',
   summary: 'Turn any Blaze template into a React component',
-  git: 'https://github.com/gwendall/meteor-blaze-to-react.git',
-  version: '1.0.0'
+  git: 'https://github.com/lablancas/meteor-blaze-to-react.git',
+  version: '1.1.0'
+});
+
+Npm.depends({
+    'react-addons-transition-group': '15.5.2',
+    'react-addons-css-transition-group': '15.5.2',
+    'react-addons-linked-state-mixin': '15.5.2',
+    'react-addons-create-fragment': '15.5.3',
+    'react-addons-update': '15.5.2',
+    'react-addons-pure-render-mixin': '15.5.2',
+    'react-addons-test-utils': '15.5.1',
+    'react-addons-perf': '15.4.2'
 });
 
 Package.onUse(function(api, where) {
-  api.versionsFrom('1.3-rc.0');
-  api.use([ 'ecmascript', 'templating', 'underscore', 'react-meteor-data' ]);
+  api.versionsFrom('1.4.4.1');
+  api.use([ 'ecmascript', 'templating', 'underscore' ]);
   api.mainModule('lib.jsx', 'client');
   api.export('BlazeToReact');
 });
